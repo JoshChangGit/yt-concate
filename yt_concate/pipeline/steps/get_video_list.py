238a6f97de4277c18b4b8d4c45cc3c0f3deb7ea2
Josh Chang <chiayu0529@gmail.com>
2021-11-08 15:00:04 +0800

    refactor code into pipeline design patten

--- yt_concate/pipeline/steps/get_video_list.py
+++ yt_concate/pipeline/steps/get_video_list.py
@@ -6,13 +6,12 @@ from yt_concate.settings import API_KEY
 
 
 class GetVideoList(Step):
-    def process(self, inputs):
+    def process(self, data, inputs):
         channel_id = inputs['channel_id']
         base_video_url = 'https://www.youtube.com/watch?v='
         base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
-        first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY,
-                                                                                                            channel_id)
+        first_url = base_search_url + f'key={API_KEY}&channelId={channel_id}&part=snippet,id&order=date&maxResults=25'
 
         video_links = []
         url = first_url
