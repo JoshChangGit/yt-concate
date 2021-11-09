5cb9eaf70eb3992a8a7acc9f54de3c64afd75cd1
Josh Chang <chiayu0529@gmail.com>
2021-11-09 18:26:33 +0800

    11/9

---
+++ yt_concate/pipeline/steps/download_captions.py
@@ -0,0 +1,29 @@
+import os
+
+from pytube import YouTube
+
+from .step import Step
+from .step import StepException
+
+import time
+
+class DownloadCaptions(Step):
+    def process(self, data, inputs, utils):
+        start = time.time()
+        for url in data:
+            if utils.caption_file_exist(url):
+                print('found existing caption file')
+                continue
+            source = YouTube(url)
+            en_caption = source.captions.get_by_language_code('a.en')
+            en_caption_convert_to_srt = (en_caption.generate_srt_captions())
+            print(en_caption_convert_to_srt)
+
+            text_file = open(utils.get_caption_path(url), "w", encoding='utf-8')
+            text_file.write(en_caption_convert_to_srt)
+            text_file.close()
+            break
+
+        end = time.time()
+        print('took', end - start, 'seconds')
+
